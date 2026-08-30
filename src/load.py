4f7e706bc95c3e7f15a8d05c1f545f203b98c240
rachardv <38317150+rachardv@users.noreply.github.com>
2026-08-29 22:11:41 -0700

    feat(postgres): migrate ibkr historical data to postgres ibkr_historical_te

--- src/load.py
+++ src/load.py
@@ -1,14 +1,14 @@
 import logging
 import pandas as pd
-import common_lib.connectors.oracle as oracle
+import common_lib.connectors.postgres as postgres
 from common_lib.config.main_config import MainConfig
 import sys
 
 
 
 def run(m_config: MainConfig, df: pd.DataFrame, write_mode: str = "upsert") -> None:
-    table_name = m_config.oracle_ibkr_ticker_table_name
-    primary_keys = oracle.get_table_metadata(m_config,table_name).get("primary_keys", [])
+    table_name = "ibkr_historical_te"
+    primary_keys = ["symbol", "datetime", "barsize"]
 
     if df.empty:
         logging.error("DataFrame is empty. Skipping DB push.")
@@ -18,7 +18,7 @@ def run(m_config: MainConfig, df: pd.DataFrame, write_mode: str = "upsert") -> N
 
 
     try:
-        oracle.insert_into_table(
+        postgres.insert_into_table(
             config=m_config,
             df=df,
             table_name=table_name,
@@ -26,10 +26,10 @@ def run(m_config: MainConfig, df: pd.DataFrame, write_mode: str = "upsert") -> N
             primary_keys=primary_keys
         )
 
-        logging.info("Load into oracle successful.")
+        logging.info("Load into PostgreSQL successful.")
 
     except Exception as e:
-        logging.error(f"Failed to push to Oracle: {e}")
+        logging.error(f"Failed to push to PostgreSQL: {e}")
         raise e
 
 
