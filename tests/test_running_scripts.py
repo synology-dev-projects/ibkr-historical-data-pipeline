d5e7846a142daef2ee035c38cf22f780acfc21b6
rachardv <38317150+rachardv@users.noreply.github.com>
2026-08-30 02:16:15 -0700

    test: isolate live tests and update pytest.ini

--- tests/test_running_scripts.py
+++ tests/test_running_scripts.py
@@ -1,17 +1,17 @@
 
 import sys
+import pytest
 import scripts.run_historical_load as calling_script
 from unittest.mock import patch
 
+@pytest.mark.integration
 def test_historical_load(env_config, pipeline_data):
     """
-    Verifies that the .env file exists and that Pydantic reads it correctly.
+    Integration test: Runs historical load script against active IBKR Gateway socket.
     """
-
-    test_args = ["run_historical_load.py", "--symbol", "SPY", "--exchange", "NASDAQ","--startDateStr","2026-02-18","--endDateStr", "2026-02-24"]
+    test_args = ["run_historical_load.py", "--symbol", "SPY", "--exchange", "NASDAQ", "--startDateStr", "2026-02-18", "--endDateStr", "2026-02-24"]
 
     with patch.object(sys, 'argv', test_args):
-
         calling_script.main()
 
 
